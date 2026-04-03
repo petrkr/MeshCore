fb118cd3d695d24b7f6049d2aa5cc1e7d6399f8a
Petr Kracik <petrkr@petrkr.net>
2026-04-03 21:33:31 +0200

    OctopusLab LAN board class with ETH support

---
+++ variants/octopuslab_lan_lora/OctopusLabLANBoard.cpp
@@ -0,0 +1,46 @@
+#ifdef ESP_PLATFORM
+
+#include "OctopusLabLANBoard.h"
+
+#if defined(ADMIN_PASSWORD) && !defined(DISABLE_WIFI_OTA)
+
+#include <ETH.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+#include <SPIFFS.h>
+
+bool OctopusLabLANBoard::startOTAUpdate(const char* id, char reply[]) {
+  if (_ota_server == nullptr) {
+    static char id_buf[60];
+    snprintf(id_buf, sizeof(id_buf), "%s (%s)", id, getManufacturerName());
+
+    static char home_buf[96];
+    snprintf(home_buf, sizeof(home_buf), "<H2>Hi! I am a MeshCore Repeater. ID: %s</H2>", id);
+
+    _ota_server = new AsyncWebServer(80);
+    _ota_server->on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+      request->send(200, "text/html", home_buf);
+    });
+    _ota_server->on("/log", HTTP_GET, [](AsyncWebServerRequest* request) {
+      request->send(SPIFFS, "/packet_log", "text/plain");
+    });
+
+    AsyncElegantOTA.setID(id_buf);
+    AsyncElegantOTA.begin(_ota_server);
+    _ota_server->begin();
+  }
+
+  IPAddress ip = ETH.localIP();
+  snprintf(reply, 160, "Started: http://%u.%u.%u.%u/update", ip[0], ip[1], ip[2], ip[3]);
+  MESH_DEBUG_PRINTLN("startOTAUpdate: %s", reply);
+  return true;
+}
+
+#else
+bool ESP32Board::startOTAUpdate(const char* id, char reply[]) {
+  return false; // not supported
+}
+
+#endif
+#endif
--- variants/octopuslab_lan_lora/target.h
+++ variants/octopuslab_lan_lora/target.h
@@ -3,12 +3,12 @@
 #define RADIOLIB_STATIC_ONLY 1
 #include <RadioLib.h>
 #include <helpers/radiolib/RadioLibWrappers.h>
-#include <helpers/ESP32Board.h>
+#include "OctopusLabLANBoard.h"
 #include <helpers/radiolib/CustomSX1276Wrapper.h>
 #include <helpers/AutoDiscoverRTCClock.h>
 #include <helpers/SensorManager.h>
 
-extern ESP32Board board;
+extern OctopusLabLANBoard board;
 extern WRAPPER_CLASS radio_driver;
 extern AutoDiscoverRTCClock rtc_clock;
 extern SensorManager sensors;
