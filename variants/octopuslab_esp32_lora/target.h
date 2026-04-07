0fa2093a10fa90196bb9cedefa14ae6b22c837ed
Petr Kracik <petrkr@petrkr.net>
2026-04-07 16:35:55 +0200

    Added OctopusLab ESP32 board

---
+++ variants/octopuslab_esp32_lora/target.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define RADIOLIB_STATIC_ONLY 1
+#include <RadioLib.h>
+#include <helpers/ESP32Board.h>
+#include <helpers/radiolib/RadioLibWrappers.h>
+#include <helpers/radiolib/CustomSX1276Wrapper.h>
+#include <helpers/AutoDiscoverRTCClock.h>
+#include <helpers/sensors/EnvironmentSensorManager.h>
+#include <helpers/ui/SSD1306Display.h>
+#include <helpers/ui/MomentaryButton.h>
+
+extern ESP32Board board;
+extern WRAPPER_CLASS radio_driver;
+extern AutoDiscoverRTCClock rtc_clock;
+extern EnvironmentSensorManager sensors;
+
+extern DISPLAY_CLASS display;
+extern MomentaryButton user_btn;
+
+bool radio_init();
+uint32_t radio_get_rng_seed();
+void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
+void radio_set_tx_power(int8_t dbm);
+mesh::LocalIdentity radio_new_identity();
